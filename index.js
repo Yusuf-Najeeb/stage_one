13d070a751d5e849dc14a721e0e74daa5812d34d
Muhammad Najeeb <yusnaj21@gmail.com>
2024-07-02 20:52:13 +0100

    Modified: Push completed version

---
+++ index.js
@@ -0,0 +1,15 @@
+const currentDay = document.getElementById("day");
+const currentTime = document.getElementById("time");
+const now = new Date();
+
+const today = now.toLocaleDateString("en-EN", { weekday: "long" });
+
+function getTime() {
+  const time = now.toUTCString().split(" ")[4];
+  currentTime.textContent = `Time: ${time}`;
+  currentDay.textContent = today;
+}
+
+getTime();
+
+setInterval(getTime, 1000);
